b050efe2ecd2d21cf2b0cce17d51f2f29a1cf5fe
D-G-Tomlinson <58551142+D-G-Tomlinson@users.noreply.github.com>
2024-05-30 19:07:41 +0100

    day4p1

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io;
 use std::cmp;
+use std::collections::HashMap;
+use std::cmp::Ordering;
 
 fn main() -> Result<(), &'static str>{
-    match day_three_part_two() {
+    match day_four_part_one() {
 	Ok(s) => println!("{s:?}"),
 	Err(e) => println!("Error in calculation. {e:?}")
     }
@@ -192,8 +194,8 @@ fn day_two_part_two() -> Result<String, String> {
     let file:io::Result<File> = File::open("puzzle-input/Day2");
     let mut contents = String::new();
     match file {
-	Ok(mut f) => _ = f.read_to_string(&mut contents),
-	Err(e) => return Err(format!("Error with file opening. {e:?}"))	    
+		Ok(mut f) => _ = f.read_to_string(&mut contents),
+		Err(e) => return Err(format!("Error with file opening. {e:?}"))	    
     }
 
     let lines = contents.lines();
@@ -285,44 +287,94 @@ fn day_three_part_one() -> Result<String, String> {
     }
     return Ok(valid.to_string());
 }
+
+
 fn day_three_part_two() -> Result<String, String> {
     let mut contents = String::new();
     match File::open("puzzle-input/Day3") {
-	Ok(mut f) => _ = f.read_to_string(&mut contents),
-	Err(e) => return Err(format!("Error with file opening. {e:?}"))	    
+		Ok(mut f) => _ = f.read_to_string(&mut contents),
+		Err(e) => return Err(format!("Error with file opening. {e:?}"))	    
     }
 
     let contents:Vec<Vec<i32>> = contents.lines().map(|l| l.split_whitespace().map(|x| x.parse().unwrap()).collect()).collect();
     let num_triples = contents.len()/3;
 
-    let mut triples:Vec<Vec<i32>> = Vec::new();
-
-    for i in 0..3 {
-	for j in 0..num_triples {
-	    let start = j * 3;
-	    triples.push(
-		vec![
-		    *contents.get(start).unwrap().get(i).unwrap(),
-		    *contents.get(start + 1).unwrap().get(i).unwrap(),
-		    *contents.get(start + 2).unwrap().get(i).unwrap()
-		]
-	    );
-	}
-    }
 
     let mut valid = 0;
-    let length = triples.len();
-  //  println!("Num triangles to check is: {length}");
-    for triplet in triples {
-//	println!("Checking triplet: {triplet:?}");
-	let v1 = *triplet.get(0).unwrap();
-	let v2 = *triplet.get(1).unwrap();
-	let v3 = *triplet.get(2).unwrap();
+    for i in 0..3 {
+		for j in 0..num_triples {
+	    	let start = j * 3;
 
-	if (v1 < v2 + v3) && (v2 < v1 + v3) && (v3 < v1 + v2) {
-	    valid = valid + 1;
-	}
-    }
-    
+			let v1 = *contents.get(start).unwrap().get(i).unwrap();
+			let v2 = *contents.get(start + 1).unwrap().get(i).unwrap();
+			let v3 = *contents.get(start + 2).unwrap().get(i).unwrap();
+
+			if (v1 < v2 + v3) && (v2 < v1+v3) && (v3 < v1 + v2) {
+				valid = valid + 1;
+			}
+		}
+	
+    }    
     return Ok(valid.to_string());
+}
+
+fn day_four_part_one() -> Result<String, String> {
+    let mut contents = String::new();
+    match File::open("puzzle-input/Day4") {
+	Ok(mut f) => _ = f.read_to_string(&mut contents),
+	Err(e) => return Err(format!("Error with file opening. {e:?}"))	    
+    }
+
+    let mut sum:i32 = 0;
+    for line in contents.lines() {
+	//	let l:String = line.chars().filter(|c| (*c != '-') && (*c != ']')).collect::<String>();
+	let l:String = line.to_string();
+	
+	let mut parts = l.split("[");
+
+	let mut encrypted_name:Vec<String> = parts.next().unwrap().split("-").map(|s| s.to_string()).collect();
+
+	let sector_id:i32 = encrypted_name.pop().expect("read sector id").parse().unwrap();
+
+	let encrypted_name:String = encrypted_name.join("");
+	let encrypted_name = encrypted_name.chars().filter(|c| (*c != '-')).collect::<String>();
+	
+	let checksum:Vec<char> = parts.next().unwrap().chars().collect();
+
+	let mut dict:HashMap<char, i32> = HashMap::new();
+
+	for c in encrypted_name.chars() {
+	    *dict.entry(c).or_insert(0) +=1;	
+	}
+
+	let mut counting_vector:Vec<_> = dict.iter().map(|(a,b)| (a, b)).collect();
+
+	fn my_compare((c1,v1):&(&char, &i32),(c2,v2):&(&char,&i32) ) -> Ordering {
+	    match v1.cmp(v2) {
+		Ordering::Greater => Ordering::Less,
+		Ordering::Less => Ordering::Greater,
+		Ordering::Equal => c1.cmp(c2)		    
+	    }
+	}
+	
+	counting_vector.sort_by(my_compare);
+//	print!("{counting_vector:?}, ");
+	let counting_vector = counting_vector.into_iter().map(|(a, b)| *a).collect::<Vec<char>>();
+
+	if counting_vector.len() >= 5 {
+	    let mut valid = true;
+	    for i in 0..5 {
+		if counting_vector[i] != checksum[i] {
+		    valid = false;
+		}
+	    }
+	    if valid {
+		sum += sector_id;
+	    }	    
+	}
+	
+//        println!("{counting_vector:?}");
+    }
+
+    return Ok(format!("{sum}"));
 }
